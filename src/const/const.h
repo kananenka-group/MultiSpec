2a6490e03fd1b488ca62f823a4a683173bcee614
Alexei Kananenka <kananenka@gmail.com>
2022-06-10 18:37:13 -0400

    added isotope exchange eq const

--- src/const/const.h
+++ src/const/const.h
@@ -9,4 +9,20 @@
 #define O_to_H_dist_water_cutoff 14.798445 //7.831 A * 1.88 conversion to au
 #define bond_plz_ratio 5.6
 
+// Equilibrium constant for: H2O + D2O = 2HOD
+// K = 3.828 is taken as an average from:
+// Table 1 of:
+// Yongdan Kim, Namkung Hankyu, and Hoeil Chung,
+// "Determination of the Equilibrium Constant of the Isotropic Disproportionation
+//  Between Water and Heavy Water Using Near-Infrared Spectroscopy,"
+//  Appl. Spectrosc. 63, 256-259 (2009)
+//
+#define  kEqIsoW  3.9
+//
+// Another possible value is 3.9 from:
+// ``Orthogonalyzed H2O and D2O species obtained from infrared spectra of liquid water at several temperatures''
+// by Jean-Joseph Max, Pascal Larouche and Camille Chapados
+// Journal of Molecular Structure 1149 (2017) 457-472
+//
+
 #endif

aa243dcef91e6127cfddb38572ca044f383a097f
Alexei Kananenka <kananenka@gmail.com>
2022-06-16 22:26:43 -0400

    added refs to OH dist cutoff

--- src/const/const.h
+++ src/const/const.h
@@ -6,7 +6,12 @@
 #define A0 1.889726125 
 
 // water module specific constants
-#define O_to_H_dist_water_cutoff 14.798445 //7.831 A * 1.88 conversion to au
+#define O_to_H_dist_water_cutoff 14.798445 
+//7.831 A * 1.88 conversion to au, 7.831 A cut-off is chosen for historical reasons
+// see: JCTC 9, 3109 (2013), JCP 128, 224511 (2008), JCP 132, 204505 (2010)
+
+// Ratio between the longitudinal and transverse bond polarizability derivatives
+// see J. G. Scherer and R. G. Snyder, J. Chem. Phys. 67, 4794 (1977).
 #define bond_plz_ratio 5.6
 
 // Equilibrium constant for: H2O + D2O = 2HOD
@@ -16,9 +21,7 @@
 // "Determination of the Equilibrium Constant of the Isotropic Disproportionation
 //  Between Water and Heavy Water Using Near-Infrared Spectroscopy,"
 //  Appl. Spectrosc. 63, 256-259 (2009)
-//
 #define  kEqIsoW  3.9
-//
 // Another possible value is 3.9 from:
 // ``Orthogonalyzed H2O and D2O species obtained from infrared spectra of liquid water at several temperatures''
 // by Jean-Joseph Max, Pascal Larouche and Camille Chapados

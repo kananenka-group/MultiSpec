6265b8a9a1e5ec8ffdc8fb46f5c53f3fb84624db
Alexei Kananenka <kananenka@gmail.com>
2022-06-17 00:47:01 -0400

    added rvec>std::vector copy

--- src/vec/vecManip.h
+++ src/vec/vecManip.h
@@ -5,6 +5,12 @@
 #include <xdrfile.h>
 #include <cstdlib>
 #include <cmath>
+#include <vector>
+
+using namespace std;
+
+inline void copyRRvec(float *out, const rvec v1)
+{ for (int ii=0; ii<DIM; ii++) out[ii]=v1[ii]; }
 
 inline void copyRvec(const rvec &M, rvec &out)
 { for (int ii=0; ii<DIM; ii++) out[ii]=M[ii]; };
@@ -22,6 +28,10 @@ inline void addRvec(const rvec &v1, rvec &v, const float &scl) {
   for (int ii=0; ii<DIM; ii++) v[ii]+=scl*v1[ii];
 };
 
+inline void sclRvec(const rvec &v1, rvec &v, const float &scl) {
+  for (int ii=0; ii<DIM; ii++) v[ii]=scl*v1[ii];
+}
+
 inline void multRvec(rvec &v, float scalar)
 { for (int ii=0; ii<DIM; ii++) v[ii]*=scalar; };
 
@@ -37,6 +47,15 @@ inline void cross(const rvec &v1, const rvec &v2, rvec &out) {
   out[1]=v1[2]*v2[0] - v1[0]*v2[2];
   out[2]=v1[0]*v2[1] - v1[1]*v2[0];
 };
+
+inline void OuterRvec(float *out, const rvec v1){
+   out[0] = v1[0]*v1[0];
+   out[1] = v1[0]*v1[1];
+   out[2] = v1[0]*v1[2];
+   out[3] = v1[1]*v1[1];
+   out[4] = v1[1]*v1[2];
+   out[5] = v1[2]*v1[2];
+}
 
 inline float norm2vec(const rvec &v) {
   float d2=0;

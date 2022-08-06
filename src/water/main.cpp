ade42500b627becded6f84958ed7e1da89feebdb
Alexei Kananenka <kananenka@gmail.com>
2022-08-06 01:52:43 -0400

    added starting frame

--- src/water/main.cpp
+++ src/water/main.cpp
@@ -24,6 +24,7 @@ int main(int argc, char ** argv){
    water S (input.getWaterModel(), input.getwSMap(), input.getwBMap(),
             input.getJobType(), input.getTrajFile(), input.getGroFile(),
             input.getAmsFile(), input.getNFrames(), input.getND2O(),
+            input.getStartFrame(),
             input.ifIR(), input.ifRaman(), input.ifSFG(), input.ifDODv(),
             input.getIntraC(), input.getInermcs(),
             input.getTDSFG(), input.getFc());

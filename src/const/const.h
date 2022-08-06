8df2c9cb7bbf49bf375dd36a92b1cd0bf8ed604e
Alexei Kananenka <kananenka@gmail.com>
2022-08-06 01:50:00 -0400

    update const.h

--- src/const/const.h
+++ src/const/const.h
@@ -1,18 +1,21 @@
 #ifndef CONST_H
 #define CONST_H
 
-#define HBAR 5.308837367 // in cm-1 * ps
-#define au_to_wn 2.19474e5
-#define A0 1.889726125 
+namespace constants
+{
+// common constants
+const double HBAR = 5.308837367; // in cm-1 * ps
+const double AU_TO_WN = 2.19474e5;
+const double A0 = 1.889726125;
 
-// water module specific constants
-#define O_to_H_dist_water_cutoff 14.798445 
+// WATER MODULE------------
 //7.831 A * 1.88 conversion to au, 7.831 A cut-off is chosen for historical reasons
 // see: JCTC 9, 3109 (2013), JCP 128, 224511 (2008), JCP 132, 204505 (2010)
+const double O_to_H_dist_water_cutoff = 14.798445;
 
 // Ratio between the longitudinal and transverse bond polarizability derivatives
 // see J. G. Scherer and R. G. Snyder, J. Chem. Phys. 67, 4794 (1977).
-#define bond_plz_ratio 5.6
+const double bond_plz_ratio = 5.6;
 
 // Equilibrium constant for: H2O + D2O = 2HOD
 // K = 3.828 is taken as an average from:
@@ -21,14 +24,30 @@
 // "Determination of the Equilibrium Constant of the Isotropic Disproportionation
 //  Between Water and Heavy Water Using Near-Infrared Spectroscopy,"
 //  Appl. Spectrosc. 63, 256-259 (2009)
-#define  kEqIsoW  3.9
+//#define  kEqIsoW  3.9
 // Another possible value is 3.9 from:
 // ``Orthogonalyzed H2O and D2O species obtained from infrared spectra of liquid water at several temperatures''
 // by Jean-Joseph Max, Pascal Larouche and Camille Chapados
 // Journal of Molecular Structure 1149 (2017) 457-472
+const double kEqIsoW = 3.9;
 
 // Switching function cut-off for SFG calculations (in A)
 // See J. Chem. Phys. 135, 044701 (2011)
-#define SWITCHF_CUT 4.0
+const double SWITCHF_CUT = 4.0;
+
+// AMIDE I MODULE----------------
+}
+
+//#define HBAR 5.308837367 // in cm-1 * ps
+//#define au_to_wn 2.19474e5
+//#define A0 1.889726125 
+
+// water module specific constants
+//#define O_to_H_dist_water_cutoff 14.798445 
+
+//#define bond_plz_ratio 5.6
+
+
+//#define SWITCHF_CUT 4.0
 
 #endif
